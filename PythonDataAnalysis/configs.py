542a00549be558c1f0f3f52398111b190dae431d
Graffslack <maksym.vette@gmail.com>
2022-02-15 13:52:35 +0100

    Analysis added on data part
    Added a few graphs for the data part and some moving averages

--- PythonDataAnalysis/configs.py
+++ PythonDataAnalysis/configs.py
@@ -1,7 +1,13 @@
 #Redis server configurations
 HOST = "95.179.129.52"
 PORT = 6379
-PASSWORD = "wowThisIsAVeryHardPasswordThatNoOneWouldBeAbleToFind69*"
+PASSWORD = "REMOVED!"
 
 #Device1 configuration settings
 DEVICENAME = "TUDelftDeviceData|testALE1|"
+
+#Special MACadress
+specialMac = "3e:9e:c1:ec:69:65"
+
+#Mobing averagefilter size
+movingAverage = 5
