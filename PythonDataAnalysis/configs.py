90a02f59e01fca7cc1aead7c13708837d77dc30e
Graffslack <maksym.vette@gmail.com>
2022-03-07 19:29:33 +0100

    added files
    added data + data_analysisi

--- PythonDataAnalysis/configs.py
+++ PythonDataAnalysis/configs.py
@@ -4,10 +4,12 @@ PORT = 6379
 PASSWORD = "hdaJA0ic6tVm7pxAZSofRugF4FPNFQVHQsQmsGQ2mB9d2eWvbSwEBhnGUbrhNATls9dQF0e8Z2tFzK4CZcpttoqzdSHMrCyIbcr"
 
 #Device1 configuration settings
+#DEVICENAME = "TUDelftDeviceData|ALE"
 DEVICENAME = "TUDelftDeviceData|App"
 
-#Special MACadress
-specialMac = "4e:9b:05:55:9f:ce"
+#Special MACadress Ale and then App
+#specialMac = "a4:50:46:4c:af:3b" 
+specialMac = "80:58:f8:93:88:29"
 
 #Mobing averagefilter size
 movingAverage = 5
@@ -19,4 +21,4 @@ OFFSETTIME = 3600000
 MACURL = "http:\\macvendors.co\api\%s"
 
 #set size of the bins in microsecs 
-BIN = 60 * 6 * 10^7
+BIN = 86400000
--- PythonDataAnalysis/dataAnalysis.py
+++ PythonDataAnalysis/dataAnalysis.py
@@ -1,3 +1,4 @@
+from cgitb import small
 from os import times
 import time
 from turtle import color
@@ -12,41 +13,69 @@ import plotly.graph_objects as go
 import requests
 from mac_vendor_lookup import MacLookup
 import json
+import sys
+import plotly.figure_factory as ff
 
 allSeenMacAdresses = []
 totalSeenMacAdresses = []
+smallestValue = 100000000000000
+highestValue = 0
 
 #Get all the data from the Redis server
 def getCurrentData():
     print("Extracting the current data from the server")
     rserver = redis.Redis(host=configs.HOST , port=configs.PORT, password=configs.PASSWORD, decode_responses=True)
-
+    global smallestValue
+    global highestValue
     #Extract the values from the server and place them in a data container
     for key in rserver.scan_iter(configs.DEVICENAME + "*"):
         timestamp = str(int(key.split('|')[2]) + configs.OFFSETTIME) 
         data = rserver.get(key) 
         dataLines = data.splitlines()
-        totalSeenMacAdresses.append({"timestamp":int(timestamp), "date": convertUnix(timestamp), "amountOfMacs": int(dataLines[0])})                   #Add the total amount of macs seen in that time stamp to the data container
-        i = 0
+        #print(timestamp)
+        if(int(timestamp)/1000 > 1645401601):
+            try:
+                
+                totalSeenMacAdresses.append({"timestamp":int(timestamp), "date": convertUnix(timestamp), "amountOfMacs": int(dataLines[0])})                   #Add the total amount of macs seen in that time stamp to the data container
+            except:
+                print("Error in txt file!")
+            i = 0
 
-        for line in dataLines:
-            if(i > 1):                                                                                                          #first item is total amount of adress and second is own macadress, should be skipped here
-                elements = line.split('|')
-                MACadress = elements[0]
-                if(any(mac.MACadress == MACadress for mac in allSeenMacAdresses)):                                              #Check if there already exists a mac object                             if(~any(frame.get("timestamp") == timestamp for frame in obj.allTimeFrames)):                               #only add new frame if the current timestamp is not already added
-                    obj = next(item for item in allSeenMacAdresses if item.MACadress == MACadress) 
-                    obj.addTimeFrame(timestamp, elements[1], elements[2], elements[3], elements[4], elements[5], elements[6])
-                else:
-                    newObj = dataFrame.DataFrame(MACadress, timestamp, elements[1], elements[2], elements[3], elements[4], elements[5], elements[6])
-                    allSeenMacAdresses.append(newObj)                     
-            i += 1
+            #Getting highest and lowest value
+            if(int(timestamp) < int(smallestValue)):
+                smallestValue = int(timestamp)
+            if(int(timestamp) > int(highestValue)):
+                highestValue = int(timestamp)
+
+            for line in dataLines:
+                if(i > 1):                                                                                                          #first item is total amount of adress and second is own macadress, should be skipped here
+                    elements = line.split('|')
+                    MACadress = elements[0]
+                    if(any(mac.MACadress == MACadress for mac in allSeenMacAdresses)):                                              #Check if there already exists a mac object                             if(~any(frame.get("timestamp") == timestamp for frame in obj.allTimeFrames)):                               #only add new frame if the current timestamp is not already added
+                        obj = next(item for item in allSeenMacAdresses if item.MACadress == MACadress) 
+                        obj.addTimeFrame(timestamp, elements[1], elements[2], elements[3], elements[4], elements[5], elements[6])
+                    else:
+                        newObj = dataFrame.DataFrame(MACadress, timestamp, elements[1], elements[2], elements[3], elements[4], elements[5], elements[6])
+                        allSeenMacAdresses.append(newObj)                     
+                i += 1
 #Method that can convert the unixtimestamp to normal time     
 def convertUnix(time):
     datetime_obj = datetime.utcfromtimestamp(int(time)/1000)                                                                    #devide by 1000 because of milli
     return datetime_obj.strftime("%d.%m.%y %H:%M:%S")
 
+def convertUnixHour(time):
+    datetime_obj = datetime.utcfromtimestamp(int(time)/1000)                                                                    #devide by 1000 because of milli
+    return datetime_obj.strftime("%d.%m.%y %H")
+
+def convertUnixDay(time):
+    datetime_obj = datetime.utcfromtimestamp(int(time)/1000)                                                                    #devide by 1000 because of milli
+    return datetime_obj.strftime("%d.%m.%y")
+
+
 #Return true when value is in dictionary False when its not the case
 def checkIfValueInListOfDict(listOfDicts, value):
+    if(len(listOfDicts) == 0):
+        return False
     for element in listOfDicts:
         if value in element.values():
             return True
@@ -59,6 +88,7 @@ def indexOfSearchedDict(listOfDicts, key, value):
         if element[key] == value:
             return i 
         i += 1
+    return -1
 
 #Calculate the moving average on a given list and return the moving average list
 def movingAverage(dataList):
@@ -85,18 +115,65 @@ def plotFigureMacadresses(listOfSpecialTimes):
     sortedList = sorted(totalSeenMacAdresses, key=lambda d: d['timestamp'])                                                     #sort the list based on the unixtimestamp
     dates, amountOfMacs, specialPoints, specialDates = [], [], [], []
 
+    """ global smallestValue
+    global highestValue
+    smallestValue 
+    UTCDays = []
+    days = []
+    dayValues = []
+    i = 0
+    while(smallestValue < highestValue):
+        UTCDays.append(smallestValue + configs.BIN)
+        days.append(convertUnixDay(smallestValue))
+        dayValues.append(0)
+        smallestValue += configs.BIN
+        i += 1
+        print(i)
+    print(UTCDays)
+    print(days)
+    print(dayValues) """
+
+    if(configs.DEVICENAME == "TUDelftDeviceData|App"):
+        UTCDays = [1645484401, 1645570801, 1645657201, 1645743601,1645830001, 1645916401, 1646002801, 1646089201, 1646175601, 1646262001]
+        days = ["21-02-2022","22-02-2022","23-02-2022","24-02-2022","25-02-2022","26-02-2022","27-02-2022","28-02-2022","01-03-2022","02-03-2022"]
+        dayValues = [0,0,0,0,0,0,0,0,0,0]
+    else:
+        UTCDays = [1645570801, 1645657201, 1645743601, 1645830001, 1645916401, 1646002801, 1646089201, 1646175601, 1646262001, 1646348401, 1646434801, 1646521201]
+        days = ["22-02-2022", "23-02-2022", "24-02-2022", "25-02-2022", "26-02-2022", "27-02-2022", "28-02-2022", "01-03-2022", "02-03-2022", "03-03-2022", "04-03-2022", "05-03-2022"]
+        dayValues = [0,0,0,0,0,0,0,0,0,0,0,0]
+        print("hello")
+
     for element in sortedList:
-        dates.append(element["date"])
-        amountOfMacs.append(element["amountOfMacs"])
+        if (element.get("timestamp")/1000 > 1645401601):
+            dates.append(element["date"])
+            amountOfMacs.append(element["amountOfMacs"])
         if listOfSpecialTimes:
-            if(str(element["timestamp"]) in listOfSpecialTimes):
+            if(str(element["timestamp"]) in listOfSpecialTimes and element.get("timestamp")/1000 > 1645401601):
                 specialPoints.append(element["amountOfMacs"])
                 specialDates.append(element["date"])
-    movingAverageList = movingAverage(amountOfMacs) 
+        i = 0        
+        for UTC in UTCDays:
+            if(element.get("timestamp")/1000 < UTC and element.get("timestamp")/1000 > 1645401601):   
+                dayValues[i] = dayValues[i] + element.get("amountOfMacs")
+                break
+            i += 1
+
+    #movingAverageList = movingAverage(amountOfMacs) 
+
+    fig = px.bar(x =days, y =dayValues, text_auto='.2s',
+                title="Amount of Macs per day")
+    fig.update_traces(textangle=0, textfont_size=14, textposition="outside", cliponaxis=False)
+    fig.update_layout(xaxis_title='Day', yaxis_title='Amount of MAC adresses')
+    fig.show()
+
+    #fig = go.Figure()
+    #fig.add_trace(go.Scatter(x=days, y=dayValues, mode='lines+markers', name='Amount of Mac adresses seen per day'))
+    #fig.update_layout(title="Amount of Mac adressess seen per day", xaxis_title='Day', yaxis_title='Amount of MAC adresses')
+    #fig.show() 
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=amountOfMacs, mode='lines+markers', name='Amount of MAC adresses', line=dict(color="#7876ff")))
-    fig.add_trace(go.Scatter(x=dates, y=movingAverageList, mode='lines', name='SMA Amount of MAC adresses'))
+    #fig.add_trace(go.Scatter(x=dates, y=movingAverageList, mode='lines', name='SMA Amount of MAC adresses'))
     fig.add_trace(go.Scatter(x=specialDates, y=specialPoints, mode='markers', name='Special MAC Adres Was Seen'))
     fig.update_layout(title="Amount of Mac adressess seen per timestamp", xaxis_title='TimeStamp', yaxis_title='Amount of MAC adresses')
     fig.show()
@@ -205,18 +282,43 @@ def printVendorSpecs():
             listOfVendors[0]['Amount']  = listOfVendors[0].get('Amount') + 1
         except:
             listOfVendors[0]['Amount']  = listOfVendors[0].get('Amount') + 1
-
+    print(listOfVendors)
     fig = px.bar(listOfVendors, y='Amount', x='Vendor', text_auto='.2s',
-                title="Default: various text sizes, positions and angles")
+                title="Amount of vendors thast were found")
     fig.update_traces(textangle=0, textfont_size=14, textposition="outside", cliponaxis=False)
     fig.show()
-    
+
+def makeDistribution():
+    distributionData = []
+    print(len(allSeenMacAdresses))
+    ones = 0
+    for macDict in allSeenMacAdresses:
+        amountSeen = int(len(macDict.allTimeFrames))
+        addElement = True
+        #print("amountseen = : " + str(amountSeen))
+        index = indexOfSearchedDict(distributionData,"TimesSeen",amountSeen)
+        if(index != -1):
+            #print("Index is : " + str(indexOfSearchedDict(distributionData,"TimesSeen",amountSeen)))
+            #print(distributionData)
+            distributionData[index]["Amount"] = int(distributionData[index].get("Amount") + 1)
+            addElement = False
+        if(addElement and amountSeen != 1):
+            newDict = {"TimesSeen": amountSeen, "Amount": int(1)}
+            distributionData.append(newDict)
+    print(distributionData)
+    df = distributionData
+
+    fig = px.histogram(df, x="TimesSeen", y = "Amount")
+
+    fig.show()
+
 def main():   
     getCurrentData()
     listOfSpecialTimes = getTimeStampsSpecialMac()
     plotFigureMacadresses(listOfSpecialTimes)
-    plotFigureDataPerTimeStamp()
+    #plotFigureDataPerTimeStamp()
     #printVendorSpecs()
+    makeDistribution()
     
 if __name__ == "__main__":
     main()
